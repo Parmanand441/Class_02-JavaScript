131ce1f9a5b705d49c0363ba59ffa994f154485d
Parmanand Rai <106175480+Parmanand441@users.noreply.github.com>
2024-03-05 17:48:30 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,53 @@
+// // operators in JS......////
+// //arithmetic operators//
+let a=10;
+let b=4;
+console.log("a+b=", a+b)
+console.log("a-b=", a-b)
+console.log("a/b=", a/b)
+console.log("a*b=", a*b)
+console.log("a%b=", a%b)
+console.log("a++ =", a++)
+console.log("++a =", ++a)
+console.log("--a =", --a)
+console.log("a-- =", a--)
+console.log("a=", a)
+
+// //assignment operators//
+
+let assignment =20;
+assignment +=5;
+console.log(assignment)
+assignment -=5;
+console.log(assignment)
+assignment *=5;
+console.log(assignment)
+assignment /= 5;
+console.log(assignment)
+assignment %=5;
+console.log(assignment)
+assignment **=7;
+console.log(assignment)
+
+//Comparison operators//
+
+let a=5;
+let b= "5";
+console.log(a==b)
+console.log(a!=b)
+console.log(a===b)
+console.log(a!==b)
+console.log(a>b)
+console.log(a<b)
+console.log(a<=b)
+console.log(a>=b)
+
+//logical operators..// &&, ||, !
+
+let a= 10;
+let b= 20;
+
+console.log(a==b && a<b)
+console.log(a>b || a<b)
+console.log(!true)
+console.log(!false)
